c956809fb7ba61f94414376f3bd2ba250d341327
lppduy <lephamphuongduy@gmail.com>
2024-05-27 19:54:34 +0700

    setup: add Joi validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,21 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
 import databaseConfig from './configs/database.config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision', 'staging')
+          .default('development'),
+        PORT: Joi.number().default(3000).required(),
+        DATABASE_PORT: Joi.number().port().required(),
+      }),
+      validationOptions: {
+        abortEarly: false,
+      },
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env',
       load: [databaseConfig],
